8b155e2c966cf96c0e6a92ef9115d0e8aab23fac
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2026-08-05 08:22:34 -0500

    add contact form

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   imports: [
-    RouterLink
+    ReactiveFormsModule
   ],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
+
+  private readonly fb = inject(FormBuilder);
+
+  submitted = false;
+
+  contactForm = this.fb.group({
+
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(100)
+      ]
+    ],
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+
+    company: [
+      '',
+      [
+        Validators.maxLength(100)
+      ]
+    ],
+
+    message: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(20),
+        Validators.maxLength(2000)
+      ]
+    ]
+
+  });
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  submit(): void {
+
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+
+      this.contactForm.markAllAsTouched();
+      return;
+
+    }
+
+    console.log('Contact Form:', this.contactForm.getRawValue());
+
+    // We'll replace this with the Firebase API call later.
+
+    this.contactForm.reset();
+
+    this.submitted = false;
+
+  }
 
 }
